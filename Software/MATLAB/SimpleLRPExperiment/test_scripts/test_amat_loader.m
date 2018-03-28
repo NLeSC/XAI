6ea82693c0f8b556ef9045fdea17d920331a2536
elboyran <e.ranguelova@esciencecenter.nl>
2018-03-28 11:54:13 +0200

    issue #3: figure formatting

--- Software/MATLAB/SimpleLRPExperiment/test_scripts/test_amat_loader.m
+++ Software/MATLAB/SimpleLRPExperiment/test_scripts/test_amat_loader.m
@@ -35,36 +35,46 @@ end
 if visualize
     % select 6 random images from the train set
     train_indicies = randi(num_train_images, 1, 6);
-    figure(1); title('Six random images from the train set');
+    figure(1);
     for i = 1:6
+        
         subplot(2,3,i);
         shape = reshape(train_images(train_indicies(i),:),32,32);
         imshow(shape);
+        if i==1
+            title('Train set random subset');
+        end        
         hold on;
     end
-    hold off;    
+    hold off;
     
     % select 6 random images from the test set
     test_indicies = randi(num_test_images, 1, 6);
-        figure(2); title('Six random images from the test set');
-    for i = 1:6
+    figure(2);
+    for i = 1:6      
         subplot(2,3,i);
         shape = reshape(test_images(test_indicies(i),:),32,32);
         imshow(shape);
+        if i==1
+            title('Test set random subset');
+        end
         hold on;
     end
-    hold off; 
+    hold off;
     
     % select 6 random images from the valid set
     valid_indicies = randi(num_val_images, 1, 6);
-    figure(3); title('Six random images from the validation set');
+    figure(3);
     for i = 1:6
         subplot(2,3,i);
         shape = reshape(valid_images(valid_indicies(i),:),32,32);
         imshow(shape);
+        if i==1
+            title('Validation set random subset');
+        end        
         hold on;
     end
-    hold off; 
+    hold off;
 end
 
 
