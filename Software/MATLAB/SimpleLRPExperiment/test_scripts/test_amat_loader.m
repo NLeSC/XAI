342fcde74740b857d45bb0582cccadc35aee1856
elboyran <e.ranguelova@esciencecenter.nl>
2018-03-28 12:19:35 +0200

    issue #3: saving the BabyAIShapes chosen dataset into MAT files and LRP format

--- Software/MATLAB/SimpleLRPExperiment/test_scripts/test_amat_loader.m
+++ Software/MATLAB/SimpleLRPExperiment/test_scripts/test_amat_loader.m
@@ -2,16 +2,31 @@
 
 %% parameters
 project_path = 'C:/Projects/eStep/XAI';
-path2BabyAIShapes = fullfile(project_path,'/Data/BabyAIShape/shapeset');
+path2originalBabyAIShapes = fullfile(project_path,'/Data/BabyAIShape/shapeset');
 train_amat_fname = 'shapeset1_1c_2s_3po.10000.train.amat';
 test_amat_fname = 'shapeset1_1c_2s_3po.5000.test.amat';
 valid_amat_fname = 'shapeset1_1c_2s_3po.5000.valid.amat';
-train_full_fname = fullfile(path2BabyAIShapes, train_amat_fname);
-test_full_fname = fullfile(path2BabyAIShapes, test_amat_fname);
-valid_full_fname = fullfile(path2BabyAIShapes, valid_amat_fname);
+train_full_fname = fullfile(path2originalBabyAIShapes, train_amat_fname);
+test_full_fname = fullfile(path2originalBabyAIShapes, test_amat_fname);
+valid_full_fname = fullfile(path2originalBabyAIShapes, valid_amat_fname);
+
+path2matBabyAIShapes = fullfile(project_path,'/Data/BabyAIShape/shapeset_mat');
+train_images_mat_fname = 'shapeset1_1c_2s_3po.10000.train_images.mat';
+test_images_mat_fname = 'shapeset1_1c_2s_3po.5000.test_images.mat';
+valid_images_mat_fname = 'shapeset1_1c_2s_3po.5000.valid_images.mat';
+train_labels_mat_fname = 'shapeset1_1c_2s_3po.10000.train_labels.mat';
+test_labels_mat_fname = 'shapeset1_1c_2s_3po.5000.test_labels.mat';
+valid_labels_mat_fname = 'shapeset1_1c_2s_3po.5000.valid_labels.mat';
+train_images_full_fname = fullfile(path2matBabyAIShapes, train_images_mat_fname);
+test_images_full_fname = fullfile(path2matBabyAIShapes, test_images_mat_fname);
+valid_images_full_fname = fullfile(path2matBabyAIShapes, valid_images_mat_fname);
+train_labels_full_fname = fullfile(path2matBabyAIShapes, train_labels_mat_fname);
+test_labels_full_fname = fullfile(path2matBabyAIShapes, test_labels_mat_fname);
+valid_labels_full_fname = fullfile(path2matBabyAIShapes, valid_labels_mat_fname);
 
 verbose = true;
 visualize = true;
+sav = true;
 
 %% loading of the files
 [train_images, train_labels, train_colors] = amat_loader(train_full_fname);
@@ -77,4 +92,22 @@ if visualize
     hold off;
 end
 
+%% saving the matricies into MAT files
+if sav
+    save(train_images_full_fname, 'train_images');
+    save(train_labels_full_fname, 'train_labels');
+    if verbose
+        disp(['Saved ', num2str(num_train_images) ,' train images and their labels in MAT files']);
+    end
+    save(test_images_full_fname, 'test_images');
+    save(test_labels_full_fname, 'test_labels');
+    if verbose
+        disp(['Saved ', num2str(num_test_images) ,' test images and their labels in MAT files']);
+    end
+    save(valid_images_full_fname, 'valid_images');
+    save(valid_labels_full_fname, 'valid_labels');    
+    if verbose
+        disp(['Saved ', num2str(num_val_images) ,' validation images and their labels in MAT files']);
+    end
+end
 
