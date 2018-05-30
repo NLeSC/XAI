e0baf25f01685b39fc00aa89161c80a5bbb6b079
elboyran <e.ranguelova@esciencecenter.nl>
2018-05-30 14:38:35 +0200

    added generic function header template

---
+++ Software/MATLAB/funciton_header_template.m
@@ -0,0 +1,23 @@
+% function_name - short function description
+% **************************************************************************
+% function [out_par1,...,out_parN] = funciton_name(in_par1,...,in_parM)
+%
+% author: Elena Ranguelova, NLeSc
+% date created: dd-mm-yyyy
+% last modification date: 
+% modification details: 
+%**************************************************************************
+% INPUTS:
+% in_parI - description of input parameter I, I=1,...,M
+%**************************************************************************
+% OUTPUTS:
+% out_parJ - description of output parameter J, J =1,...,N
+%**************************************************************************
+% NOTES: 
+%**************************************************************************
+% EXAMPLES USAGE: 
+% 
+% see test_script_name.m
+%**************************************************************************
+% REFERENCES:
+%**************************************************************************
