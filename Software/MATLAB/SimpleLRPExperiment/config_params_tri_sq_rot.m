369fd1888edbcdfe11cf376b5fb7c3df214e841f
elboyran <e.ranguelova@esciencecenter.nl>
2018-06-08 17:20:07 +0200

    issue #17: adding band masks

--- Software/MATLAB/SimpleLRPExperiment/config_params_tri_sq_rot.m
+++ Software/MATLAB/SimpleLRPExperiment/config_params_tri_sq_rot.m
@@ -20,15 +20,22 @@ end
 train_images_mat_fname = 'TrianglesAndSquares_images_train_50k.mat';
 test_images_mat_fname = 'TrianglesAndSquares_images_test_30k.mat';
 valid_images_mat_fname = 'TrianglesAndSquares_images_valid_20k.mat';
+
 train_labels_mat_fname = 'TrianglesAndSquares_labels_train_50k.mat';
 test_labels_mat_fname = 'TrianglesAndSquares_labels_test_30k.mat';
 valid_labels_mat_fname = 'TrianglesAndSquares_labels_valid_20k.mat';
+
+test_bands_mat_fname = 'TrianglesAndSquares_bands_test_30k.mat';
+
 train_images_full_fname = fullfile(path2matTrianglesAndSquaresShapes, train_images_mat_fname);
 test_images_full_fname = fullfile(path2matTrianglesAndSquaresShapes, test_images_mat_fname);
 valid_images_full_fname = fullfile(path2matTrianglesAndSquaresShapes, valid_images_mat_fname);
+
 train_labels_full_fname = fullfile(path2matTrianglesAndSquaresShapes, train_labels_mat_fname);
 test_labels_full_fname = fullfile(path2matTrianglesAndSquaresShapes, test_labels_mat_fname);
 valid_labels_full_fname = fullfile(path2matTrianglesAndSquaresShapes, valid_labels_mat_fname);
+
+test_bands_full_fname = fullfile(path2matTrianglesAndSquaresShapes, test_bands_mat_fname);
 
 
 path2models = fullfile(project_path, '/Results/Models');
@@ -73,6 +80,3 @@ res_order = [2 1 3];
 % see the comments of issue #9: https://github.com/NLeSC/XAI/issues/9
 bg_point = [1 1];
 fg_point = [15 12];
-
-%% define evidence threshold
-eps = 0.0001;
