8af74ba0e04b8a1485e5f2060da5f921f02d53ce
Joost Berkhout <j.berkhout@cwi.nl>
2018-10-26 11:53:55 +0200

    Construction of a perfect neural network with 99.99667% accuracy on test dataset.

--- Software/Python/SimpleLRPExperiments/modules/binstep.py
+++ Software/Python/SimpleLRPExperiments/modules/binstep.py
@@ -5,7 +5,14 @@ Created on Thu Oct 25 18:43:55 2018
 Author: Joost Berkhout (CWI, email: j.berkhout@cwi.nl)
 
 Description: Contains the binary step activation layer.
+
 See also: https://en.wikipedia.org/wiki/Heaviside_step_function
+
+Two options are implemented: the true bin step function and an
+analytical approximation as given on Wikipedia. The idea of the
+analytical approximation is that its derivative exists, making it
+more smooth.
+
 """
 
 import numpy as np
@@ -21,12 +28,27 @@ class BinStep(Module):
     def __init__(self):
         Module.__init__(self)
         self.layerName = 'BinStep'
+        self.K = 100000  # large constant
+        self.eps = 1.0/self.K  # very small constant
 
     def forward(self,X):
-        self.Y = 1*(X >= 0)
+
+        self.X = X
+
+#        # analytic approximation of the bin step function
+#        self.Y = .5 + .5*np.tanh(self.K*X)
+#        return self.Y
+
+         # true bin step function:
+        self.Y = 1*(X >= -self.eps)
         return self.Y
 
     def backward(self,DY):
+
+#        # analytic approximation of the bin step function
+#        return DY*.5*self.K*(1.0-self.Y**2)
+
+        # true bin step function:
         return 0*DY
 
     def clean(self):
