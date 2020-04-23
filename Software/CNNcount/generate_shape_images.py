7a9e8402374134d6a9468f17a6a4e807576f8c44
elboyran <e.ranguelova@esciencecenter.nl>
2020-04-23 13:39:39 +0200

    issue #52

---
+++ Software/CNNcount/generate_shape_images.py
@@ -0,0 +1,42 @@
+import numpy as np
+from skimage.morphology import disk, diamond, square
+import matplotlib.pyplot as plt
+
+def my_square(radius, dtype):
+    return square(radius*2+1, dtype)
+
+SHAPES = [disk, diamond, my_square]
+
+def generate_image(img_size, shapes, allow_occlusion=2):
+    img = np.zeros((img_size, img_size), bool)
+    i = 0
+    while i < len(shapes):
+        shape_type_ind, radius = shapes[i]
+        shape_type = SHAPES[shape_type_ind]
+        shape = shape_type(radius, bool)
+        shape_area = np.sum(shape)
+        x, y = np.random.randint(radius, img_size-radius, 2)
+        xi, xf = x-radius, x+radius+1
+        yi, yf = y-radius, y+radius+1
+
+        ori = img[xi:xf, yi:yf]
+        res = np.logical_or(ori, shape)
+        if np.sum(res)-np.sum(ori) >= shape_area - allow_occlusion:
+            img[xi:xf, yi:yf] = res
+            i += 1
+    return img
+
+if __name__ == '__main__':
+    NIM = 10 #nbr of images
+    NMIN, NMAX = 1, 3  # nbr of shapes
+    RMIN, RMAX = 2, 6  # shape radius
+
+    for im in range(NIM):
+        n = int(np.random.randint(NMIN, NMAX+1))
+        shapes = [(np.random.randint(len(SHAPES)), np.random.randint(RMIN, RMAX))
+                  for i in range(n)]
+    
+        img = generate_image(64, shapes)
+        plt.imshow(img)
+        plt.title('n = %d' % n)
+        plt.show()
