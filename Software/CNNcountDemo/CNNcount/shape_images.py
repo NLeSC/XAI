e117a74d308469e339cfc891ac0ff1d9dea26819
elboyran <e.ranguelova@esciencecenter.nl>
2020-04-29 18:02:00 +0200

    issue #50: Loading and splitting the date to train, validaiton and testing

--- Software/CNNcountDemo/CNNcount/shape_images.py
+++ Software/CNNcountDemo/CNNcount/shape_images.py
@@ -7,6 +7,8 @@ def my_square(radius, dtype):
     return square(radius*2+1, dtype)
 
 SHAPES = [disk, diamond, my_square]
+TRAIN = 0.7
+VALID = 0.2
 
 # generating a single binary shape image
 def generate_image(img_size, shapes, allow_occlusion=2):
@@ -27,6 +29,39 @@ def generate_image(img_size, shapes, allow_occlusion=2):
             img[xi:xf, yi:yf] = res
             i += 1
     return img
+
+# load and split to training, test and validaiton data
+def load_split_data(fname):
+    
+    # load
+    data = np.load(fname)
+    images=data['images']
+    labels=data['labels']
+        
+    # split      
+    nim = len(labels)
+    ntr = int(TRAIN*nim)
+    nval = int(VALID*nim)
+   # ntst = nim - (ntr + nval)
+   # print("#train, #validation and #test: ", ntr, nval, ntst)
+    
+    end_train_ind = ntr 
+    #print("start train ind: beginning",",  end train ind: ", end_train_ind)
+    images_train = images[:end_train_ind]
+    labels_train = labels[:end_train_ind]
+    
+    start_val_ind = end_train_ind 
+    end_val_ind = start_val_ind + nval
+    #print("start valid ind: ", start_val_ind, ", end valid ind: ", end_val_ind)
+    images_val = images[start_val_ind:end_val_ind]    
+    labels_val = labels[start_val_ind:end_val_ind]
+    
+    start_test_ind = end_val_ind
+    #print("start test ind:", start_test_ind, ", end test ind: end")
+    images_test = images[start_test_ind:]
+    labels_test = labels[start_test_ind:]
+    
+    return images_train, images_val, images_test, labels_train, labels_val, labels_test
 
 # plot 12 random shape images
 def plot_12images(images, labels):
