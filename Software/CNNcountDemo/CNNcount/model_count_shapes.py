0dff88b5c1c4ac7ff426a35971402f65ef455f3b
elboyran <e.ranguelova@esciencecenter.nl>
2020-05-11 18:38:37 +0200

    issue#50. Explicit layer names and retraining of Model1

--- Software/CNNcountDemo/CNNcount/model_count_shapes.py
+++ Software/CNNcountDemo/CNNcount/model_count_shapes.py
@@ -8,16 +8,18 @@ from keras.layers import Conv2D, MaxPooling2D
 # generate the model
 def generate_cnncount_model(input_shape, num_classes):
     model = Sequential()
+    # inninvestigate complains about defaut layer names, so give the layers explicit names
     model.add(Conv2D(32, kernel_size=(3, 3),
                      activation='relu',
-                     input_shape=input_shape))
-    model.add(Conv2D(64, (3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-    model.add(Flatten())
-    model.add(Dense(128, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(num_classes, activation='softmax'))
+                     input_shape=input_shape,
+                     name='conv2d_layer1'))
+    model.add(Conv2D(64, (3, 3), activation='relu', name='conv2d_layer2'))
+    model.add(MaxPooling2D(pool_size=(2, 2), name='maxpooling2d_layer1'))
+    model.add(Dropout(0.25, name='dropout_layer1'))
+    model.add(Flatten(name='flatten_layer1'))
+    model.add(Dense(128, activation='relu', name='dense_layer1'))
+    model.add(Dropout(0.5, name='dropout_layer2'))
+    model.add(Dense(num_classes, activation='softmax', name='dense_layer2'))
     
     return model
 
