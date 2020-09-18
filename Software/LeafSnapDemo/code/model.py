4b1f3aa88c313110c1439bc1a624254c7a8e20e3
elboyran <e.ranguelova@esciencecenter.nl>
2020-09-18 18:55:07 +0200

    full model as simpler one doesn't work better

--- Software/LeafSnapDemo/code/model.py
+++ Software/LeafSnapDemo/code/model.py
@@ -9,6 +9,26 @@ from keras.layers import Conv2D, MaxPooling2D
 def generate_model(input_shape, num_classes):
     # inninvestigate complains about defaut layer names, so give the layers explicit names
     model = Sequential()
+    model.add(Conv2D(32, kernel_size=(5, 5), 
+                     strides=(1, 1), 
+                     activation='relu',
+                     input_shape=input_shape,
+                     name='conv2d_layer1'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='maxpooling2d_layer1'))
+    model.add(Conv2D(64, (5, 5), activation='relu', name='conv2d_layer2'))
+    model.add(MaxPooling2D(pool_size=(2, 2), name='maxpooling2d_layer2'))
+    model.add(Dropout(0.7, name='dropout_layer1'))
+    model.add(Flatten(name='flatten_layer1'))
+    model.add(Dense(1000, activation='relu', name='dense_layer1'))
+    model.add(Dropout(0.7, name='dropout_layer2'))
+    model.add(Dense(num_classes, activation='softmax', name='dense_layer2'))
+    
+    return model
+
+# generate a very simple model
+def generate_simple_model(input_shape, num_classes):
+    # inninvestigate complains about defaut layer names, so give the layers explicit names
+    model = Sequential()
     model.add(Conv2D(16, kernel_size=(5, 5), 
                      strides=(1, 1), 
                      activation='relu',
@@ -19,29 +39,11 @@ def generate_model(input_shape, num_classes):
     model.add(MaxPooling2D(pool_size=(2, 2), name='maxpooling2d_layer2'))
     model.add(Dropout(0.5, name='dropout_layer1'))
     model.add(Flatten(name='flatten_layer1'))
-    model.add(Dense(500, activation='relu', name='dense_layer1'))
+    model.add(Dense(200, activation='relu', name='dense_layer1'))
     model.add(Dropout(0.5, name='dropout_layer2'))
     model.add(Dense(num_classes, activation='softmax', name='dense_layer2'))
     
-    return model
 
-# generate a very simple model
-def generate_simple_model(input_shape, num_classes):
-    # inninvestigate complains about defaut layer names, so give the layers explicit names
-    model = Sequential()
-    # inninvestigate complains about defaut layer names, so give the layers explicit names
-    model.add(Conv2D(16, kernel_size=(5, 5),
-                     activation='relu',
-                     input_shape=input_shape,
-                     name='conv2d_layer1'))
-    model.add(Conv2D(32, (5, 5), activation='relu', name='conv2d_layer2'))
-    model.add(MaxPooling2D(pool_size=(2, 2), name='maxpooling2d_layer1'))
-    model.add(Dropout(0.25, name='dropout_layer1'))
-    model.add(Flatten(name='flatten_layer1'))
-    model.add(Dense(100, activation='relu', name='dense_layer1'))
-    model.add(Dropout(0.5, name='dropout_layer2'))
-    model.add(Dense(num_classes, activation='softmax', name='dense_layer2'))
-    
     return model
 
 # train the model
